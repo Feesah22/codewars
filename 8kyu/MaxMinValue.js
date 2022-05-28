93304f7cf4ff364cc6d2f78327524a17d1973a5e
feesah22 <nafeesah.q.shah@gmail.com>
2022-05-28 01:39:02 -0400

    progress

---
+++ 8kyu/MaxMinValue.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/577a98a6ae28071780000989
+//Find Maximum Values of a List
+
+// Your task is to make two functions(max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+//     Examples(Input -> Output)
+
+
+const min = (list) => Math.min(...list)
+const max = (list) => Math.max(...list)
