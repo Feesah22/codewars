9826f9a95a5f01853b6901e2e021b90c56cdb446
feesah22 <nafeesah.q.shah@gmail.com>
2022-05-28 00:00:02 -0400

    update2

---
+++ 8kyu/stringendswith.js
@@ -0,0 +1,8 @@
+//String ends with 
+
+function solution(str, ending) {
+    //   if(ending.length == 0) return true
+    //  return str.substr(-(ending.length)) == ending
+    return str.endsWith(ending)
+
+}
